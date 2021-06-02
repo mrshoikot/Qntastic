9f4a2a81184afb3a74b9842addb39583d0dde49a
mrshoikot <mrshoikot@gmail.com>
2021-06-02 22:55:24 +0600

    Add project files.

---
+++ Qmanager/CreateEntry.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Qmanager
+{
+    /// <summary>
+    /// Interaction logic for CreateEntry.xaml
+    /// </summary>
+    public partial class CreateEntry : Window
+    {
+        public CreateEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            EnterButton.Foreground = Brushes.Black;
+        }
+    }
+}
